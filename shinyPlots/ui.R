be36fde9f603224d1077b97e187c8ddc24ffc1d7
Jherson Sazo <Jherson.sazo@galileo.edu>
2021-12-04 12:04:43 -0600

    Tarea completada

--- shinyPlots/ui.R
+++ shinyPlots/ui.R
@@ -6,25 +6,14 @@ library(lubridate)
 
 shinyUI(
   fluidPage(
-   
-    titlePanel("Interacciones de usuario con graficas"),
-    tabsetPanel(
-      tabPanel('plot',
-               h1('Graficas en Shiny'),
-               plotOutput('grafica_base_r'),
-               plotOutput('grafica_ggplot')
-               ),
-      tabPanel('Clicks en plots',
-               plotOutput("plot_click_options",
-                          click = 'clk',
-                          dblclick = 'dclik',
-                          hover='mouse_hover',
-                          brush='mouse_brush'
-                          ),
-               verbatimTextOutput("output_1"),
-               DT::dataTableOutput("mtcars_tbl")
-             )
-    )
+    fluidRow(
+      h1("Grafico Dinamico"),
+      plotOutput("plot", brush = "plot_brush", dblclick = "plot_reset", click = "click", hover = "hover")
+    ),
     
+    fluidRow(
+      h1("Tabla Dinamica"),
+      dataTableOutput("tabla")
+    )
   )
 )
